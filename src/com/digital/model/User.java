c8cf68afee939bb2edf64f7236b9286e8729f77b
aravind <aravind@aravind>
2017-03-08 15:28:57 +0530

    commit by aravind

---
+++ src/com/digital/model/User.java
@@ -0,0 +1,30 @@
+package com.digital.model;
+
+public class User {
+	private int id;	
+private String firstname;
+private String lastname;
+private String email;
+private String alternatemail;
+private String password;
+private String confirmpassword;
+public String getfirstName() {
+	return firstname;
+}
+public void setlastName(String name) {
+	this.lastname = name;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+
+}
