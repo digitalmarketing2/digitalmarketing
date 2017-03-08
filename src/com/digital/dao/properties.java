001d7e14584a83cbe5b9663bd460ed0f24c4c2ff
aravind <aravind@aravind>
2017-03-08 16:31:00 +0530

    by aravind

--- src/com/digital/dao/properties.java
+++ src/com/digital/dao/properties.java
@@ -1,5 +1,9 @@
 package com.digital.dao;
 
-public interface properties {
 
-}
+	public interface properties {
+		String DRIVER="com.mysql.jdbc.Driver";
+	    String URL="jdbssc:mysql://localhost:3306/visog";
+	    String USER ="root";
+		String PASSWORD="123visog";
+	}
