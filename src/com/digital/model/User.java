ac694bde71490c63436f567e3512537b40f59c4c
Sri Hari <Sri Hari@192.168.0.31>
2017-03-08 15:56:18 +0530

    by srihari

--- src/com/digital/model/User.java
+++ src/com/digital/model/User.java
@@ -1,30 +1,82 @@
 package com.digital.model;
 
 public class User {
-	private int id;	
-private String firstname;
-private String lastname;
-private String email;
-private String alternatemail;
-private String password;
-private String confirmpassword;
-public String getfirstName() {
-	return firstname;
-}
-public void setlastName(String name) {
-	this.lastname = name;
-}
-public String getEmail() {
-	return email;
-}
-public void setEmail(String email) {
-	this.email = email;
-}
-public String getPassword() {
-	return password;
-}
-public void setPassword(String password) {
-	this.password = password;
-}
+	private int Id;	
+public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getFirstName() {
+		return FirstName;
+	}
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+	public String getLastName() {
+		return LastName;
+	}
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+	public String getDOB() {
+		return DOB;
+	}
+	public void setDOB(String dOB) {
+		DOB = dOB;
+	}
+	public String getGender() {
+		return Gender;
+	}
+	public void setGender(String gender) {
+		Gender = gender;
+	}
+	public String getMobilenumber() {
+		return Mobilenumber;
+	}
+	public void setMobilenumber(String mobilenumber) {
+		Mobilenumber = mobilenumber;
+	}
+	public String getCountry() {
+		return Country;
+	}
+	public void setCountry(String country) {
+		Country = country;
+	}
+	public String getState() {
+		return State;
+	}
+	public void setState(String state) {
+		State = state;
+	}
+	public String getCity() {
+		return City;
+	}
+	public void setCity(String city) {
+		City = city;
+	}
+private String FirstName;
+private String LastName;
+private String Email;
+private String Password;
 
+private String DOB;
+private String Gender;
+private String Mobilenumber;
+private String Country;
+private String State;
+private String City;
 }
